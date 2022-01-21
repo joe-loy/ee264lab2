405c5f6bc944e7526882a924a68453c29d88c538
Joe Loy <37566258+joe-loy@users.noreply.github.com>
2022-01-20 22:44:20 -0800

    Update ZeroOrderHoldInterp.cpp

--- EE264_lab_app/Lab2/ZeroOrderHoldInterp.cpp
+++ EE264_lab_app/Lab2/ZeroOrderHoldInterp.cpp
@@ -20,15 +20,17 @@ void ZeroOrderHoldInterp::filter(int16_t *outputData,
     
     // Upsample input data and apply zero-order hold filter - Lab 2.1
     // Hint: use tempData array and check that is large enough to hold upsampled data
+    // size of the temp array is compared before upSample and is not performed if it cannot 
+    // fit into the temp array. This means are samples are limited to lengths of 8192 samples. 
     
-    for(int i = 0; i < inputNumSamples; i++) {
-        if (sizeof(tempData) > inputNumSamples * upSampleFactor) {
+    if (sizeof(tempData) > inputNumSamples * upSampleFactor) { 
+        for(int i = 0; i < inputNumSamples; i++) {
             for (int j = 0; j < upSampleFactor; j++) {
                 tempData[i * upSampleFactor + j] = inputData[i]    
             }
-        }
-           
+        }  
     }
+    
 
     // Downsample output of zero-order hold filter - Lab 2.3
     for (int i = 0; i < inputNumSamples * upSampleFactor / downSampleFactor;  i++) {
